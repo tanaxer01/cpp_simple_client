8ce89eb36bfea16e5d1ec37cd9233f7f762e744e
tanaxer01 <jpbrione@gmail.com>
2022-11-10 19:31:25 -0300

    Starting commit

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main() {
     "accept: application/json",
     "Notion-Version: 2022-06-28",
     "content-type: application/json",
-    "authorization: Bearer secret_24HWAknnpVQIRKGNmRNv2PllffnbIrpWh3BfDjTZ6HO"
+    "authorization: "
   };
 
   long b = a->Get("https://api.notion.com/v1/blocks/cf33ad4bc76440f1a568e7e4cd5281c5/children", headers);
